6a015ac7a346f6461e6f3a57410f3d2d3bd75016
amir <amirinbarcode@gmail.com>
2022-01-23 11:14:13 +0200

    almost finish carousel

--- src/components/PrimaryWine.jsx
+++ src/components/PrimaryWine.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react';
-// import coin from '../assets/imgs/icons/';
 export const PrimaryWine = () => {
-  // const SVG = require.content('../assets/imgs/icons', true, /\.svg$/);
-  // console.log(SVG);
-  const [selectColor, setSelectColor] = useState(0);
-
+  const [selectColor, setSelectColor] = useState(1);
   const selectPrice = (priceNum) => {
     setSelectColor(priceNum);
   };
-
+  const wines = [1, 2, 3, 4, 5, 6];
   return (
     <section className="primary-wines">
       <div className="primary-top-title">Top lists in your area</div>
@@ -20,18 +16,156 @@ export const PrimaryWine = () => {
           style={{
             backgroundImage:
               selectColor === 1
-                ? require('../assets/imgs/icons/coin.svg')
-                : require('../assets/imgs/icons/coinwhite.svg'),
+                ? `url(${
+                    require('../assets/imgs/icons/coinwhite.svg').default
+                  })`
+                : `url(${require('../assets/imgs/icons/coin.svg').default})`,
+
+            backgroundColor: selectColor === 1 ? '#ba1628' : '',
           }}
         ></button>
         <button
           className="price-btn mid-price"
           onClick={() => selectPrice(2)}
+          style={{
+            backgroundImage:
+              selectColor === 2
+                ? `url(${
+                    require('../assets/imgs/icons/doublecoinwhite.svg').default
+                  })`
+                : `url(${
+                    require('../assets/imgs/icons/doublecoin.svg').default
+                  })`,
+
+            backgroundColor: selectColor === 2 ? '#ba1628' : '',
+          }}
         ></button>
         <button
           className="price-btn hi-price"
           onClick={() => selectPrice(3)}
+          style={{
+            backgroundImage:
+              selectColor === 3
+                ? `url(${
+                    require('../assets/imgs/icons/threecoinwhite.svg').default
+                  })`
+                : `url(${
+                    require('../assets/imgs/icons/threecoin.svg').default
+                  })`,
+
+            backgroundColor: selectColor === 3 ? '#ba1628' : '',
+          }}
         ></button>
+      </div>
+      <div className="wines-carousel flex">
+        <div className="carousel-control">
+          <svg class="chevron__chevron--2zNRX undefined" viewBox="0 0 7 14">
+            <g>
+              <path
+                d="M-0.000,13.281 L6.568,7.001 L-0.000,0.722 "
+                fill="none"
+                stroke="#585858"
+                stroke-width="1"
+              ></path>
+            </g>
+          </svg>
+        </div>
+        <div className="wine-card">
+          <div className="upper-card flex ">
+            <div className="wine-img hover-wine">
+              <img src={require('../assets/imgs/icons/1wine.png')} alt="" />
+            </div>
+            <div className="main-rating flex column align-center ">
+              <div className="rating flex column align-center">
+                <div className="avg-rate">4.2</div>
+                <div>stars</div>
+                <div>8183 ratings</div>
+              </div>
+              <div style={{ color: '#b03000' }}>Sold Out</div>
+            </div>
+          </div>
+          <div className="wine-details">
+            <p>Pasqua Vigneti e Cantine</p>
+            <p>Desire Lush & Zin</p>
+            <p>Primitivo 2018</p>
+          </div>
+          <div className="wine-location flex align-center ">
+            <img src={require('../assets/imgs/icons/flags/italy.png')} alt="" />
+            <span>Puglia, Italy</span>
+          </div>
+        </div>
+        <div className="wine-card">
+          <div className="upper-card flex ">
+            <div className="wine-img hover-wine">
+              <img src={require('../assets/imgs/icons/1wine.png')} alt="" />
+            </div>
+            <div className="main-rating flex column align-center ">
+              <div className="rating flex column align-center">
+                <div className="avg-rate">4.2</div>
+                <div>stars</div>
+                <div>8183 ratings</div>
+              </div>
+              <div style={{ color: '#b03000' }}>Sold Out</div>
+            </div>
+          </div>
+          <div className="wine-details">
+            <p>Pasqua Vigneti e Cantine</p>
+            <p>Desire Lush & Zin</p>
+            <p>Primitivo 2018</p>
+          </div>
+          <div className="wine-location flex align-center ">
+            <img src={require('../assets/imgs/icons/flags/italy.png')} alt="" />
+            <span>Puglia, Italy</span>
+          </div>
+        </div>
+        <div className="wine-card">
+          <div className="upper-card flex ">
+            <div className="wine-img hover-wine">
+              <img src={require('../assets/imgs/icons/1wine.png')} alt="" />
+            </div>
+            <div className="main-rating flex column align-center ">
+              <div className="rating flex column align-center">
+                <div className="avg-rate">4.2</div>
+                <div>stars</div>
+                <div>8183 ratings</div>
+              </div>
+              <div style={{ color: '#b03000' }}>Sold Out</div>
+            </div>
+          </div>
+          <div className="wine-details">
+            <p>Pasqua Vigneti e Cantine</p>
+            <p>Desire Lush & Zin</p>
+            <p>Primitivo 2018</p>
+          </div>
+          <div className="wine-location flex align-center ">
+            <img src={require('../assets/imgs/icons/flags/italy.png')} alt="" />
+            <span>Puglia, Italy</span>
+          </div>
+        </div>
+        <div className="wine-card">
+          <div className="upper-card flex ">
+            <div className="wine-img hover-wine">
+              <img src={require('../assets/imgs/icons/1wine.png')} alt="" />
+            </div>
+            <div className="main-rating flex column align-center ">
+              <div className="rating flex column align-center">
+                <div className="avg-rate">4.2</div>
+                <div>stars</div>
+                <div>8183 ratings</div>
+              </div>
+              <div style={{ color: '#b03000' }}>Sold Out</div>
+            </div>
+          </div>
+          <div className="wine-details">
+            <p>Pasqua Vigneti e Cantine</p>
+            <p>Desire Lush & Zin</p>
+            <p>Primitivo 2018</p>
+          </div>
+          <div className="wine-location flex align-center ">
+            <img src={require('../assets/imgs/icons/flags/italy.png')} alt="" />
+            <span>Puglia, Italy</span>
+          </div>
+        </div>
       </div>
     </section>
   );
