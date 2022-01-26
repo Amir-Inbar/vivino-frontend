dc6db736c6587a3c8114df1eb74eb382cff23760
amir <amirinbarcode@gmail.com>
2022-01-26 23:35:33 +0200

    adding store

---
+++ src/store/actions/reviewAction.js
@@ -0,0 +1,13 @@
+import { reviewService } from '../../services/review.service';
+
+export function loadReviews() {
+  return async (dispatch, getState) => {
+    const { filterBy } = getState().review.Module;
+    try {
+      const reviews = await reviewService.query(filterBy);
+      dispatch({ type: 'SET_REVIEWS', reviews });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import { reviewReducer } from './reducers/reviewReducer';
 import { wineReducer } from './reducers/wineReducer';
 import { wineryReducer } from './reducers/wineryReducer';
 
@@ -8,6 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const rootReducer = combineReducers({
   wineModule: wineReducer,
   wineryModule: wineryReducer,
+  reviewModule: reviewReducer,
 });
 
 export const store = createStore(
