64f938b8d2b0041594b85d32ddc5fac1d2b0312e
amir <amirinbarcode@gmail.com>
2022-02-02 21:54:09 +0200

    adding rates option

--- src/components/ReviewStat.jsx
+++ src/components/ReviewStat.jsx
@@ -2,6 +2,13 @@ import { tryRequire } from '../services/util.service';
 import { WineRate } from './WineRatePreview';
 
 export const ReviewStat = ({ wine }) => {
+  console.log('wine :>> ', wine);
+
+  const setRatingBar = (rating) => {
+    if (!parseInt(rating)) return 0;
+    console.log(parseInt(rating) / wine.ratings);
+    return parseInt(rating) / wine.ratings;
+  };
   let totalStars = 6;
   let total = 5;
   return (
@@ -9,18 +16,23 @@ export const ReviewStat = ({ wine }) => {
       <WineRate rate={wine.rate} ratings={wine.ratings} />
       <div className="saperate"></div>
       <div className="rate-line flex column ">
-        {[...Array(total)].map(() => (
+        {[...Array(total)].map((el, idx) => (
           <div className="flex" style={{ 'margin-block-end': '20px' }}>
             <div className="stars flex">
               {[...Array((totalStars -= 1))].map(() => (
                 <img src={tryRequire('imgs/icons/single-star.svg')} alt="" />
               ))}
             </div>
-
             <div className="background-bar">
-              <div className="background-fill"></div>
+              <div
+                className="background-fill"
+                style={{
+                  width: setRatingBar(`${wine[`rate${idx + 1}`]}`) * 134 + 'px',
+                  height: '16px',
+                }}
+              ></div>
             </div>
-            <span> {wine.ratings}</span>
+            <span>{`${wine[`rate${idx + 1}`]}`}</span>
           </div>
         ))}
       </div>
