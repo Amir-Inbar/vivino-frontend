6c9f5ad675643b049e76946e19d6ca1e6be4910a
zivtal <zivtal83@gmail.com>
2022-02-03 15:04:46 +0200

    review stat fixed

--- src/components/ReviewStat.jsx
+++ src/components/ReviewStat.jsx
@@ -1,40 +1,43 @@
-import { tryRequire } from '../services/util.service';
-import { WineRate } from './WineRatePreview';
+import { tryRequire } from "../services/util.service";
+import { WineRate } from "./WineRatePreview";
 
 export const ReviewStat = ({ wine }) => {
-  console.log('wine :>> ', wine);
+  console.log("wine :>> ", wine);
 
   const setRatingBar = (rating) => {
     if (!parseInt(rating)) return 0;
     console.log(parseInt(rating) / wine.ratings);
     return parseInt(rating) / wine.ratings;
   };
-  let totalStars = 6;
-  let total = 5;
+  const total = 5;
   return (
     <div className="review-stat">
       <WineRate rate={wine.rate} ratings={wine.ratings} />
       <div className="saperate"></div>
       <div className="rate-line flex column ">
-        {[...Array(total)].map((el, idx) => (
-          <div className="flex" style={{ 'margin-block-end': '20px' }}>
-            <div className="stars flex">
-              {[...Array((totalStars -= 1))].map(() => (
-                <img src={tryRequire('imgs/icons/single-star.svg')} alt="" />
-              ))}
+        {[...Array(total)].map((el, idx) => {
+          return (
+            <div className="flex" style={{ "margin-block-end": "20px" }}>
+              <div className="stars flex">
+                {[...Array(total - idx)].map(() => (
+                  <img src={tryRequire("imgs/icons/single-star.svg")} alt="" />
+                ))}
+              </div>
+              <div className="background-bar">
+                <div
+                  className="background-fill"
+                  style={{
+                    width:
+                      setRatingBar(`${wine[`rate${total - idx}`]}`) * 134 +
+                      "px",
+                    height: "16px",
+                  }}
+                ></div>
+              </div>
+              <span>{`${wine[`rate${total - idx}`]}`}</span>
             </div>
-            <div className="background-bar">
-              <div
-                className="background-fill"
-                style={{
-                  width: setRatingBar(`${wine[`rate${idx + 1}`]}`) * 134 + 'px',
-                  height: '16px',
-                }}
-              ></div>
-            </div>
-            <span>{`${wine[`rate${idx + 1}`]}`}</span>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
