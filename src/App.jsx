40216e4923f10c48e0d99bfe6ae8e78aceb936c1
amir <amirinbarcode@gmail.com>
2022-02-08 08:11:43 +0200

    adding route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
-import { AppHeader } from "./components/AppHeader";
-import { WinePage } from "./pages/WinePage";
-import { WineryPage } from "./pages/WineryPage";
-import { HomePage } from "./pages/HomePage";
-import "./styles/global.scss";
-import { LoginPage } from "./pages/LoginPage";
-import { FilterPage } from "./pages/WineFilterPage";
-import { useDispatch, useSelector } from "react-redux";
-import { wineService } from "./services/wine.service";
-import { setKeywords } from "./store/actions/wineAction";
-import { useEffect } from "react";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import { AppHeader } from './components/AppHeader';
+import { WinePage } from './pages/WinePage';
+import { WineryPage } from './pages/WineryPage';
+import { HomePage } from './pages/HomePage';
+import { WineEditPage } from './pages/WineEditPage';
+import { LoginPage } from './pages/LoginPage';
+import { FilterPage } from './pages/WineFilterPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { wineService } from './services/wine.service';
+import { setKeywords } from './store/actions/wineAction';
+import { useEffect } from 'react';
+import './styles/global.scss';
 
 export function App() {
   const keywords = useSelector((state) => state.wineModule.keywords);
@@ -35,6 +36,7 @@ export function App() {
           <Switch>
             <Route component={WinePage} path="/wine/:id" />
             <Route component={FilterPage} path="/wine" />
+            <Route component={WineEditPage} path="/addwine" />
             <Route component={WineryPage} path="/winery/:id" />
             <Route component={LoginPage} path="/login" />
             <Route component={HomePage} path="/" />
