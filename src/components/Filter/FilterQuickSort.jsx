550959a4ac57db0ffc49f7ca9ea457a80fcaba83
zivtal <zivtal83@gmail.com>
2022-02-09 14:41:22 +0200

    sort filter screen dimm

--- src/components/Filter/FilterQuickSort.jsx
+++ src/components/Filter/FilterQuickSort.jsx
@@ -9,9 +9,12 @@ export const FilterQuickSort = () => {
   const [isActive, setIsActive] = useState(false);
   const sort = useSelector((state) => state.wineModule.sort);
 
-  const SortMenu = ({ isActive, el }) => {
+  const SortMenu = ({ isActive, el, close }) => {
+    console.dir(el);
     const top = el?.offsetTop + el?.clientHeight + 16;
     const width = Math.max(el?.clientWidth || 0, 150);
+    const right = window.innerWidth - (el?.offsetLeft + el?.clientWidth);
+    const height = document.documentElement.scrollHeight;
     const options = [
       { title: "rate" },
       { title: "popular", key: "ratings" },
@@ -33,19 +36,26 @@ export const FilterQuickSort = () => {
     };
     return isActive ? (
       <div
-        className="quick-sort hover-box"
-        style={{ top: `${top}px`, width: `${width}px` }}
+        className="background-dimm"
+        style={{ height: height + "px" }}
+        onClick={close}
       >
-        {options.map(({ title, key }, idx) => (
-          <button
-            key={`SORT_BUTTON_${idx}`}
-            onClick={() => toggleSelect(key || title)}
-            className={style(key || title)}
-          >
-            {title || key}
-            {position(key || title)}
-          </button>
-        ))}
+        <div
+          onClick={(ev) => ev.stopPropagation()}
+          className="quick-sort hover-box"
+          style={{ top: `${top}px`, right: `${right}px`, width: `${width}px` }}
+        >
+          {options.map(({ title, key }, idx) => (
+            <button
+              key={`SORT_BUTTON_${idx}`}
+              onClick={() => toggleSelect(key || title)}
+              className={style(key || title)}
+            >
+              {title || key}
+              {position(key || title)}
+            </button>
+          ))}
+        </div>
       </div>
     ) : null;
   };
@@ -72,7 +82,11 @@ export const FilterQuickSort = () => {
       >
         sort
       </button>
-      <SortMenu isActive={isActive} el={elButton.current} />
+      <SortMenu
+        isActive={isActive}
+        el={elButton.current}
+        close={() => setIsActive(false)}
+      />
     </>
   );
 };
